041f1897273d1bc63b6125eb3558691ab137a160
Mattt Thompson <m@mattt.me>
2012-12-09 11:01:48 -0800

    Bumping version to 0.2.0

--- lib/rack/subdomain.rb
+++ lib/rack/subdomain.rb
@@ -1,6 +1,6 @@
 module Rack
   class Subdomain
-    VERSION = '0.1.0'
+    VERSION = '0.2.0'
 
     def initialize(app, domain, options = {}, &block)
       # Maintain compatibility with previous rack-subdomain gem
